9887dd5a4467cfcb1a4572b4bd5203b9b80e7137
CalebCX <690209522@qq.com>
2021-02-04 17:01:57 +0800

    Commit files

---
+++ HelloGit/src/main/java/com/chen/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.chen.git;
+
+/**
+ * @author Caleb Chen
+ * @create 2021-02-04 16:54
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
