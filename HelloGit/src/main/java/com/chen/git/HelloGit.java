7f5026286dd08f66a279708fb8c19076c5b1d673
CalebCX <690209522@qq.com>
2021-02-04 17:35:08 +0800

    克隆者添加

--- HelloGit/src/main/java/com/chen/git/HelloGit.java
+++ HelloGit/src/main/java/com/chen/git/HelloGit.java
@@ -12,5 +12,6 @@ public class HelloGit {
         System.out.println("主干添加");
         System.out.println("分支添加");
         System.out.println("线上更新");
+        System.out.println("克隆项目添加");
     }
 }
